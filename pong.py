dddb4c94b1104ee493a9346ddbe7e751632756a6
ThePurox <nico@localhost.localdomain>
2018-01-26 23:04:10 +0100

    update

--- pong.py
+++ pong.py
@@ -1,23 +1,61 @@
 import pygame, sys
 from pygame.locals import *
-OSZI = False # outbut is Oszi or Pygame
+import time
+OSZI = False # output is Oszi or Pygame
 if OSZI:
     import serial
 
 
-pygame.init()
 
 BGCOLOR = (100,50,50)
 LINECOLOR = (000,255,000)
 WINDOWHEIGHT = 400
 WINDOWWIDTH = 400
+BARSIZE = WINDOWHEIGHT//5
 pos = [WINDOWWIDTH//2,WINDOWHEIGHT//2]
 length = 100
 
+playerList = []
+playerLeftPos = [0,(WINDOWHEIGHT-BARSIZE)//2]
+playerRightPos = [WINDOWWIDTH-1,(WINDOWHEIGHT-BARSIZE)//2]
+
+leftUp = K_d
+leftDown = K_f
+rightUp = K_k
+rightDown = K_j
 
 if OSZI:
     ser=serial.Serial('/dev/ttyACM0', 115200, timeout=10)
 
+
+class player:
+    pos = []
+    movedir = 0
+    controls = []
+    def __init__(self,left):
+        movedir = 0
+        if left:
+            self.pos = playerLeftPos
+            self.controls = [leftUp,leftDown]
+        else:
+            self.pos = playerRightPos
+            self.controls = [rightUp,rightDown]
+    def changeVel(self,dir,key):
+        if key == self.controls[0]: #accelerate up
+            self.movedir -= 1*dir
+        if key == self.controls[1]: #accelerate down
+            self.movedir += 1*dir
+    def move(self):
+        self.pos[1] += self.movedir*2
+    def draw(self):
+        line(self.pos,[self.pos[0],self.pos[1]+BARSIZE])
+
+
+# init Game
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH , WINDOWHEIGHT))
+playerList.append(player(True))
+playerList.append(player(False))
 
 def line(start_pos,end_pos):
     if OSZI:
@@ -25,31 +63,42 @@ def line(start_pos,end_pos):
     pygame.draw.line(DISPLAYSURF,LINECOLOR,start_pos,end_pos)
 
 def clearscreen():
-    line([65535,0],[0,0])
-
-
+    if OSZI:
+        line([65535,0],[0,0])
+    DISPLAYSURF.fill(BGCOLOR)
 
 def terminate():
     pygame.quit()
     sys.exit()
 
-def drawPlayer():
+def drawGame():
     clearscreen()
-    line(pos,[pos[0]+length,pos[1]+length])
+    for player in playerList:
+        player.draw()
+
+def doGameStep():
+    for p in playerList:
+        p.move()
 
 
-DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH , WINDOWHEIGHT))
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
             terminate()
         if event.type == KEYDOWN:
-            print("keydown")
-            pos[0] += 5;
-            pos[1] += 5;
-    drawPlayer()
+            for p in playerList:
+                if event.key in p.controls:
+                    p.changeVel(1, event.key)
+        if event.type == KEYUP:
+            for p in playerList:
+                if event.key in p.controls:
+                    p.changeVel(-1, event.key)
+
+    doGameStep()
+    drawGame()
     pygame.display.update()
+    time.sleep(1/60)
 
 
 if OSZI:
