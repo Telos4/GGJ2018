64d46aa6577bab82df3a9336b02a811e99fb4455
ThePurox <nico@localhost.localdomain>
2018-01-26 21:24:09 +0100

    update

--- pong.py
+++ pong.py
@@ -1,12 +1,13 @@
 import pygame, sys
 from pygame.locals import *
-import serial
+#import serial
 
-ser=serial.Serial('/dev/ttyACM0', 115200, timeout=10)
+#ser=serial.Serial('/dev/ttyACM0', 115200, timeout=10)
 
 
 def line(a,b,c,d):
-    ser.write(((a<<48)+(b<<32)+(c<<16)+(d)).to_bytes(8,'big'))
+#    ser.write(((a<<48)+(b<<32)+(c<<16)+(d)).to_bytes(8,'big'))
+    return
 
 def clearscreen():
     line(65535,0,0,0)
@@ -27,7 +28,7 @@ def drawPlayer():
     line(pos[0],pos[1],pos[0]+length,pos[1]+length)
 
 
-
+DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH , WINDOWHEIGHT))
 
 while True:
     for event in pygame.event.get():
@@ -38,4 +39,4 @@ while True:
     drawPlayer()
 
 
-ser.close()
+#ser.close()
