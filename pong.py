06ad17b4cc09c9bed55abb644af5b7060699919e
Daniel Heinlein <dhein112@gmail.com>
2018-01-26 20:55:22 +0100

    pong.py

--- pong.py
+++ pong.py
@@ -1,6 +1,18 @@
 import pygame, sys
 from pygame.locals import *
 import subprocess
+import serial
+
+ser=serial.Serial('/dev/cu.usbmodem3297371', 115200, timeout=10)
+
+
+def line(a,b,c,d):
+    ser.write(((a<<48)+(b<<32)+(c<<16)+(d)).to_bytes(8,'big'))
+
+def clearscreen():
+    line(65535,0,0,0)
+
+
 
 OSZI = False # outbut is Oszi or Pygame
 pygame.init()
@@ -9,6 +21,7 @@ BGCOLOR = (100,50,50)
 pos = [200,200]
 
 def drawPlayer():
+    clearscreen()
     line(pos[0],pos[1],pos[0]+30,pos[1]+30)
 
 
@@ -20,3 +33,6 @@ while True:
             pos[0] += 5;
             pos[1] += 5;
     drawPlayer()
+
+
+ser.close()
