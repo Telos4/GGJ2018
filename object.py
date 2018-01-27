0ff781703f679ea68ff6a7139500d21adfd31c39
spirkelmann <simon.pirkelmann@gmail.com>
2018-01-27 22:15:03 +0100

    restructured collision detection for easier implementation of more physical collisions

--- object.py
+++ object.py
@@ -46,11 +46,17 @@ class BALL(Object):
 
     def __init__(self,  renderer, pos=[0,0], size=[5,5]):
         Object.__init__(self,pos=pos, renderer=renderer)
+
         self.pos = np.array([self.renderer.windowwidth/2,self.renderer.windowheight/2])
         self.size = size
-        self.movedir = np.array([random(),random()])
+
         self.velBallDefault = 20
+
+
+        # physical parameters of the ball
         self.vel = self.velBallDefault
+        self.mass = 1.0
+        self.movedir = np.array([random(), random()])
 
         if self.renderer.oszi == True:
             self.velMax = 100
@@ -61,38 +67,59 @@ class BALL(Object):
     def move(self,obstacleList):
         posOld = self.pos
         posNew = posOld + self.movedir/np.linalg.norm(self.movedir)*self.vel
+
         # collision with obstacles:
         u0 = posOld[0]
         v0 = posOld[1]
         u1 = posNew[0]
         v1 = posNew[1]
         mb = (v1-v0)/(u1-u0)
+
+        # find closest colliding object
+        min_distance = 1.0e+9
+        min_collision_object = None
         for obst in obstacleList:
             x0 = obst.start[0]
             y0 = obst.start[1]
             x1 = obst.end[0]
             y1 = obst.end[1]
-            mo = (y1-y0)/(x1-x0)
-            x = (y0-v0+mb*u0-mo*x0)/(mb-mo)
-            y = y0 + mo*(x-x0)
-            cross = np.array([x,y])
+            mo = (y1 - y0) / (x1 - x0)
+            x = (y0 - v0 + mb * u0 - mo * x0) / (mb - mo)
+            y = y0 + mo * (x - x0)
+            cross = np.array([x, y])
+            current_distance = np.linalg.norm(cross - posOld)
             if min(u0,u1) < x < max(u0,u1):
-                if min(y0,y1) < y < max(y0,y1): # if collision occurs
+                if min(y0,y1) < y < max(y0,y1): # if collision occurs remember the object
+                    if current_distance < min_distance:
+                        min_distance = current_distance
+                        min_collision_object = obst
 
-                    # push obstacle out of the way
-                    obst.vel += self.vel
-                    obst.movedir = self.movedir
+            # if a collision occurred handle it
+            if not (min_collision_object == None):
+                obst = min_collision_object
+                x0 = obst.start[0]
+                y0 = obst.start[1]
+                x1 = obst.end[0]
+                y1 = obst.end[1]
+                mo = (y1 - y0) / (x1 - x0)
+                x = (y0 - v0 + mb * u0 - mo * x0) / (mb - mo)
+                y = y0 + mo * (x - x0)
+                cross = np.array([x, y])
 
-                    a = posNew-cross
-                    b = np.array([x1-x0,y1-y0])
-                    b = b/np.linalg.norm(b)
-                    X = np.dot(a,b)*b
-                    posMirror = 2*cross+2*X-posNew
-                    posNew = posMirror
-                    self.movedir = posMirror-cross
+                # push obstacle out of the way
+                obst.vel += self.vel
+                obst.movedir = self.movedir
 
-                    # collision with obstacle speedup and enforcing of maximum velocity
-                    self.vel = min(self.vel * self.speedup, self.velMax)
+                a = posNew-cross
+                b = np.array([x1-x0,y1-y0])
+                b = b/np.linalg.norm(b)
+                X = np.dot(a,b)*b
+                posMirror = 2*cross+2*X-posNew
+                posNew = posMirror
+                self.movedir = posMirror-cross
+
+                # collision with obstacle speedup and enforcing of maximum velocity
+                self.vel = min(self.vel * self.speedup, self.velMax)
 
 
         self.pos = posNew
